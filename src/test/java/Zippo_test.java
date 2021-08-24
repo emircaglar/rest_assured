ac04c8bb54c67ca924e2fe49b7ab8631e2145edc
emircaglar <emircaglar2906@gmail.com>
2021-08-24 20:30:34 +0200

    ilk ayarlar

---
+++ src/test/java/Zippo_test.java
@@ -0,0 +1,20 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Zippo_test {
+
+    @Test
+    public void test(){
+
+        given()// hazirlik
+
+        .when()// link ve aksiyon
+
+        .then()// test ve extract
+        ;
+
+    }
+}
