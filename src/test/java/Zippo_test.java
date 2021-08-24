3612b5c7eb8efcc6e184ecd397921b91e6f6d8aa
emircaglar <emircaglar2906@gmail.com>
2021-08-24 21:09:58 +0200

    status code 200

--- src/test/java/Zippo_test.java
+++ src/test/java/Zippo_test.java
@@ -8,13 +8,24 @@ public class Zippo_test {
 
     @Test
     public void test(){
-
         given()// hazirlik
 
         .when()// link ve aksiyon
 
         .then()// test ve extract
         ;
+    }
+     @Test
+    public void statusCodeTest(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()// man kann alle body schauen
+                .statusCode(200)//Wenn wir 201 schreiben, bekommen wir dieses Satz :Expected status code <201> but was <200>.
+        ;
+
 
     }
 }
