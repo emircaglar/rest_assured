59e9e348f4030575cae9904443c806d5667bd530
emircaglar <emircaglar2906@gmail.com>
2021-08-26 21:39:57 +0200

    Places

---
+++ src/test/java/POJO/Location.java
@@ -0,0 +1,6 @@
+package POJO;
+
+public class Location {
+
+
+}
---
+++ src/test/java/POJO/Places.java
@@ -0,0 +1,66 @@
+package POJO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Places {
+    String placename;
+    String longitude;
+    String state;
+    String latitude;
+    String stateabbreviation;
+
+    @JsonProperty("place name")
+    public void setPlacename(String placename) {
+        this.placename = placename;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+    @JsonProperty("statea bbreviation")
+    public void setStateabbreviation(String stateabbreviation) {
+        this.stateabbreviation = stateabbreviation;
+    }
+
+
+    public String getPlacename() {
+        return placename;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getStateabbreviation() {
+        return stateabbreviation;
+    }
+
+    @Override
+    public String toString() {
+        return "Places{" +
+                "placename='" + placename + '\'' +
+                ", longitude='" + longitude + '\'' +
+                ", state='" + state + '\'' +
+                ", latitude='" + latitude + '\'' +
+                ", stateabbreviation='" + stateabbreviation + '\'' +
+                '}';
+    }
+
+
+}
