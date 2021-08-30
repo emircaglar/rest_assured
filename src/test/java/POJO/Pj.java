a0a85edff1b7e986eff58acc004740c0d381738e
emircaglar <emircaglar2906@gmail.com>
2021-08-30 21:32:01 +0200

    POJO Tasks

---
+++ src/test/java/POJO/Pj.java
@@ -0,0 +1,68 @@
+package POJO;
+
+public class Pj {
+
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    private Integer userId;
+
+    @Override
+    public String toString() {
+        return "Pj{" +
+                "userId=" + userId +
+                ", id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+
+    private String id;
+    private String title;
+    private boolean completed;
+
+
+
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/Task.java
@@ -0,0 +1,138 @@
+import POJO.Location;
+import POJO.Pj;
+import io.restassured.http.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Task {
+
+    /**
+     * Task 1
+     * create a request to https://httpstat.us/203
+     * expect status 203
+     * expect content type TEXT
+     */
+
+    @Test
+    public void test1() {
+
+        given()
+                .when()
+                .get(" https://httpstat.us/203")
+                .then()
+                .statusCode(203)
+                .contentType(ContentType.TEXT)
+
+        ;
+
+    }
+
+    /**
+     * Task 2
+     * create a request to https://httpstat.us/203
+     * expect status 203
+     * expect content type TEXT
+     * expect BODY to be equal to "203 Non-Authoritative Information"
+     */
+
+    @Test
+    public void test2() {
+
+        given()
+                .when()
+                .get(" https://httpstat.us/203")
+                .then()
+                .statusCode(203)
+                .body(equalTo("203 Non-Authoritative Information"))
+        ;
+    }
+    /** Task 3
+     *  create a request to https://jsonplaceholder.typicode.com/todos/2
+     *  expect status 200
+     *  expect content type JSON
+     *  expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+    @Test
+    public void test3() {
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title",equalTo("quis ut nam facilis et officia qui"))
+        ;
+    }
+    /** Task 4
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     *  expect status 200
+     *  expect content type JSON
+     *  expect response completed status to be false
+     */
+    @Test
+    public void test4() {
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .log().body()
+                .body("completed",equalTo(false))
+        ;
+    }
+
+    /** Task 5
+     * create a request to https://jsonplaceholder.typicode.com/todos
+     * expect status 200
+     * expect content type JSON
+     * expect third item have:
+     *      title = "fugiat veniam minus"
+     *      userId = 1
+     */
+    @Test
+    public void test5() {
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .log().body()
+                .body("title",hasItem("fugiat veniam minus"))
+                .body("userId",hasItem(1))
+        ;
+    }
+    /** Task 6
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * Converting Into POJO
+     */
+
+    @Test
+    public void test6() {
+       Pj pj=
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .log().body()
+                .extract().as(Pj.class)
+        ;
+
+        System.out.println("pj.getId() = " + pj.getId());
+        System.out.println("pj.getTitle() = " + pj.getTitle());
+        System.out.println("pj.isCompleted() = " + pj.isCompleted());
+        System.out.println("pj.getUserId() = " + pj.getUserId());
+
+    }
+}
