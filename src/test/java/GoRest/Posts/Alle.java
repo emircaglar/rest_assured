c3ff8a20279ec25368dd22b082bc4a0b27aa8808
emircaglar <emircaglar2906@gmail.com>
2021-09-09 15:33:04 +0200

    gorest post weiter

--- src/test/java/GoRest/Posts/Alle.java
+++ src/test/java/GoRest/Posts/Alle.java
@@ -9,7 +9,28 @@ public class Alle {
     private Meta meta;
     private Data data;
 
+    @Override
+    public String toString() {
+        return "Alle{" +
+                "meta=" + meta +
+                ", data=" + data +
+                '}';
+    }
 
+    public Meta getMeta() {
+        return meta;
+    }
 
+    public void setMeta(Meta meta) {
+        this.meta = meta;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
 
 }
--- src/test/java/GoRest/User/goRest_Post_Test.java
+++ src/test/java/GoRest/User/goRest_Post_Test.java
@@ -1,15 +1,18 @@
 package GoRest.User;
 
 
+import GoRest.Posts.Alle;
 import GoRest.Posts.Data;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-
+import static org.hamcrest.Matchers.*;
 public class goRest_Post_Test {
 
 
@@ -27,33 +30,47 @@ public class goRest_Post_Test {
                         .statusCode(200)
                         .contentType(ContentType.JSON)
                         .extract().response();
-
-        List<Data> data_class=response.jsonPath().getList("data",Data.class);
-        System.out.println("data_class = " + data_class);
+       List<Data> data_class=response.jsonPath().getList("data",Data.class);
+        int limit=response.jsonPath().getInt("meta.pagination.limit");
+        System.out.println("limit = " + limit);
+        for (Data data:data_class
+             ) {
+            System.out.println("data = " + data);
+        }
+      /*  */
+        
 
     }
 
     int _id ;
     @Test(priority = 2)
     public void goRestC_Post_create() {
-    Data data=new Data();
-    data.setTitle("title1");
-    data.setBody("title1 1111111");
 
-         _id =
+        String title="bu gunler gecer1";
+        String body="bu gunler tabiki gecer1";
+    Data data=new Data();
+    data.setTitle(title);
+    data.setBody(body);
+
+         Response response=
                 given()
                         .header("Authorization", "Bearer e77d719430c52f24f35e308c36023cfcd90108263e454b1fe8ebda8221624570")
                         .contentType(ContentType.JSON)
-
+                        .body(data)
                         .when()
-                        .post("https://gorest.co.in/public/v1/posts")
+                        .post("https://gorest.co.in/public/v1/users/87/posts")
                         .then()
-                        //.log().body()
+                        .log().body()
                         .statusCode(201)
                         .contentType(ContentType.JSON)
-                        .extract().jsonPath().getInt("data.user_id");
+                        .body("data.title",equalTo(title))
+                        .body("data.body",equalTo(body))
+                        .extract().response();
 
-        System.out.println("_id = " + _id);
+
+        _id=response.jsonPath().getInt("data.id");
+        Assert.assertTrue(data.getTitle().equals(response.jsonPath().getString("data.title")));
+        Assert.assertTrue(data.getBody().equals(response.jsonPath().getString("data.body")));
 
     }
 
@@ -78,5 +95,45 @@ public class goRest_Post_Test {
         }
 
     }
+    @Test(dependsOnMethods = "goRestC_Post_create")
+    public void goRestC_Post_aCreate_post() {
 
+                given()
+                        .header("Authorization", "Bearer e77d719430c52f24f35e308c36023cfcd90108263e454b1fe8ebda8221624570")
+                        .contentType(ContentType.JSON)
+                        .pathParam("id",_id)
+                        .when()
+                        .get("https://gorest.co.in/public/v1/posts/{id}")
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .contentType(ContentType.JSON)
+
+                ;
+
+    }
+
+
+    @Test(dependsOnMethods = "goRestC_Post_create")
+    public void goRestC_Post_aCreate_uodate() {
+
+        String body="bu gunler tabiki gecer gecer de nasil gecer1";
+
+
+        given()
+                .header("Authorization", "Bearer e77d719430c52f24f35e308c36023cfcd90108263e454b1fe8ebda8221624570")
+                .contentType(ContentType.JSON)
+                .pathParam("id",_id)
+                .body("{\"body\":\"" + body + "\"}")
+                .when()
+                .put("https://gorest.co.in/public/v1/posts/{id}")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+        .body("data.body",equalTo(body))
+
+        ;
+
+    }
 }
